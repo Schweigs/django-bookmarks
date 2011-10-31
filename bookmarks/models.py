ea40181306f2a67359f5373150d2604fb2e57429
Patrick Altman <paltman@gmail.com>
2011-10-31 13:47:24 -0500

    Update for new coding standards, removed Feed, and changed to django-taggit

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 import urlparse
 
 from django.db import models
@@ -6,8 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from django.contrib.auth.models import User
 
-from tagging.fields import TagField
-from tagging.models import Tag
+from taggit.managers import TaggableManager
 
 """
 A Bookmark is unique to a URL whereas a BookmarkInstance represents a
@@ -25,54 +24,47 @@ bookmark but allows for per-user tagging.
 class Bookmark(models.Model):
     
     url = models.URLField(unique=True)
-    description = models.CharField(_('description'), max_length=100)
-    note = models.TextField(_('note'), blank=True)
+    description = models.CharField(_("description"), max_length=100)
+    note = models.TextField(_("note"), blank=True)
     
-    has_favicon = models.BooleanField(_('has favicon'))
-    favicon_checked = models.DateTimeField(_('favicon checked'), default=datetime.now)
+    has_favicon = models.BooleanField(_("has favicon"))
+    favicon_checked = models.DateTimeField(_("favicon checked"), default=datetime.datetime.now)
     
-    adder = models.ForeignKey(User, related_name="added_bookmarks", verbose_name=_('adder'))
-    added = models.DateTimeField(_('added'), default=datetime.now)
+    adder = models.ForeignKey(User, related_name="added_bookmarks", verbose_name=_("adder"))
+    added = models.DateTimeField(_("added"), default=datetime.datetime.now)
     
-    # tags = TagField()
     
     def get_favicon_url(self, force=False):
         """
         return the URL of the favicon (if it exists) for the site this
         bookmark is on other return None.
         
-        If force=True, the URL will be calculated even if it doesn't
+        If force=True, the URL will be calculated even if it does not
         exist.
         """
         if self.has_favicon or force:
-            base_url = '%s://%s' % urlparse.urlsplit(self.url)[:2]
-            favicon_url = urlparse.urljoin(base_url, 'favicon.ico')
+            base_url = "%s://%s" % urlparse.urlsplit(self.url)[:2]
+            favicon_url = urlparse.urljoin(base_url, "favicon.ico")
             return favicon_url
         return None
-    
-    def all_tags(self, min_count=False):
-        return Tag.objects.usage_for_model(BookmarkInstance, counts=False, min_count=None, filters={'bookmark': self.id})
-    
-    def all_tags_with_counts(self, min_count=False):
-        return Tag.objects.usage_for_model(BookmarkInstance, counts=True, min_count=None, filters={'bookmark': self.id})
     
     def __unicode__(self):
         return self.url
     
     class Meta:
-        ordering = ('-added', )
+        ordering = ["-added", ]
 
 
 class BookmarkInstance(models.Model):
     
-    bookmark = models.ForeignKey(Bookmark, related_name="saved_instances", verbose_name=_('bookmark'))
-    user = models.ForeignKey(User, related_name="saved_bookmarks", verbose_name=_('user'))
-    saved = models.DateTimeField(_('saved'), default=datetime.now)
+    bookmark = models.ForeignKey(Bookmark, related_name="saved_instances", verbose_name=_("bookmark"))
+    user = models.ForeignKey(User, related_name="saved_bookmarks", verbose_name=_("user"))
+    saved = models.DateTimeField(_("saved"), default=datetime.datetime.now)
     
-    description = models.CharField(_('description'), max_length=100)
-    note = models.TextField(_('note'), blank=True)
+    description = models.CharField(_("description"), max_length=100)
+    note = models.TextField(_("note"), blank=True)
     
-    tags = TagField()
+    tags = TaggableManager()
     
     def save(self, force_insert=False, force_update=False):
         try:
@@ -91,4 +83,4 @@ class BookmarkInstance(models.Model):
             bookmark.delete()
     
     def __unicode__(self):
-        return _("%(bookmark)s for %(user)s") % {'bookmark':self.bookmark, 'user':self.user}
+        return _("%(bookmark)s for %(user)s") % {"bookmark":self.bookmark, "user":self.user}
